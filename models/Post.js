5605763f3a75a3260a8decbc96d6c749cadbdaea
Lê Nhật Minh <lenhatminh11a1@gmail.com>
2022-01-27 10:03:49 +0700

    adding Post Schema

---
+++ models/Post.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  avatar: String,
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+      },
+      avatar: String,
+      date: {
+        type: String,
+        default: Date.now,
+      },
+    },
+  ],
+});
+
+module.exports = mongoose.model("post", PostSchema);
